f4f62149784311f07f6ac4f3d6e264d9043ec40a
PhilippThoelke <philipp.thoelke@posteo.de>
2023-07-15 02:15:18 -0400

    Add test for dirty insertion

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -10,5 +10,17 @@ def test_duplicate_output_address():
     data = dict()
     p1.update(data)
     with pytest.raises(RuntimeError):
-        # this should raise an error because p1 and p2 have the same output address
+        # this should raise because p1's and p2's features have the same address
         p2.update(data)
+
+
+def test_dirty_duplicate_output_address():
+    p1 = DummyProcessor(".*", output_address="dummy")
+    p2 = DummyProcessor(".*", output_address="dummy")
+
+    data = dict()
+    p1.update(data)
+    data["/dummy/feature"].dirty = True
+
+    # this should NOT raise an error because we set p1's output to dirty
+    p2.update(data)
